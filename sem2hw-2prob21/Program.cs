9d29db6dca31ba1ce30f2e99f3e0b140c6783c2e
PetrHatters <psy.entry@gmail.com>
2023-06-18 14:47:59 +0400

    HW done

---
+++ sem2hw-2prob21/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+Задача 21
+Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+A (3,6,8); B (2,1,-7), -> 15.84
+A (7,-5, 0); B (1,-1,9) -> 11.53
+*/ 
+Console.Write("Введине X для первой точки: ");
+double x1 = double.Parse(Console.ReadLine());
+Console.Write("Введине Y для первой точки: ");
+double y1 = double.Parse(Console.ReadLine());
+Console.Write("Введине Z для первой точки: ");
+double z1 = double.Parse(Console.ReadLine());
+
+Console.Write("Введине X для второй точки: ");
+double x2 = double.Parse(Console.ReadLine());
+Console.Write("Введине Y для второй точки: ");
+double y2 = double.Parse(Console.ReadLine());
+Console.Write("Введине Z для второй точки: ");
+double z2 = double.Parse(Console.ReadLine());
+
+double s = Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2) + Math.Pow(z1 - z2, 2));
+Console.WriteLine($"Расстояние между точками = {s:f2}");
---
+++ sem3hw-1prob19/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+Задача 19
+Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+14212 -> нет
+12821 -> да
+23432 -> да
+*/ 
+Console.WriteLine("Введите 5-значное число: ");
+int input = int.Parse(Console.ReadLine());
+int inputSafe = input;
+int[] arr = new int[input.ToString().Length];
+int[] copy = new int[arr.Length];
+for(int i=0; i<arr.Length; i++)
+{
+    arr[i] = input%10;
+    copy[i]= input%10;
+    input/=10;
+}
+Console.WriteLine(" ");
+Array.Reverse(arr);
+bool isEqual = Enumerable.SequenceEqual(arr, copy);
+Console.Write($"{inputSafe} - палиндром? - "); 
+Console.WriteLine(isEqual);
